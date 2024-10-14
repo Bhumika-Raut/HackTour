f970bc2c4cef02ad10db88ded51d5fe8ef66f3bf
Bhumika-Raut <bhumikaraut59620@gmail.com>
2024-10-14 15:46:01 +0530

    test4

--- Server/savedSchema.js
+++ Server/savedSchema.js
@@ -1,11 +1,12 @@
 const mongoose = require('mongoose');
 
-const savedSchema = new mongoose.Schema({
-    title: String,
-    description: String,
-    image: String,
-    userId: { type: mongoose.Schema.Types.ObjectId, ref: 'Account' }, // Add a reference to the Account
+const savedEntitySchema = new mongoose.Schema({
+    userId: { type: String, required: true },
+    name: { type: String, required: true },
+    description: { type: String },
+    createdAt: { type: Date, default: Date.now },
+    // Add other fields as necessary
 });
 
-const SavedEntity = mongoose.model('SavedEntity', savedSchema);
+const SavedEntity = mongoose.model('SavedEntity', savedEntitySchema);
 module.exports = SavedEntity;
