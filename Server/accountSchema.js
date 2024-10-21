fd8892e921be16e1921ff96b6b56e519705614c4
Bhumika-Raut <bhumikaraut59620@gmail.com>
2024-10-21 14:59:26 +0530

    Profile login

--- Server/accountSchema.js
+++ Server/accountSchema.js
@@ -1,10 +1,12 @@
 const mongoose = require('mongoose');
 
+// Define the Account schema
 const accountSchema = new mongoose.Schema({
     name: { type: String, required: true, unique: true }, // Ensure name is unique
-    password: { type: String, required: true },
-    profileImage: { type: String, required: true },
+    password: { type: String, required: true },           // Password is required
+    profileImage: { type: String, required: true },       // Profile image (URL) is required
 });
 
+// Create and export the Account model
 const Account = mongoose.model('Account', accountSchema);
 module.exports = Account;
