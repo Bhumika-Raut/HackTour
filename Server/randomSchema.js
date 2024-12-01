b5a897cd8bbe986b73d3d96f4f8beaa859c6f2fd
Bhumika-Raut <bhumikaraut59620@gmail.com>
2024-12-01 22:30:14 +0530

    working on likes

--- Server/randomSchema.js
+++ Server/randomSchema.js
@@ -1,16 +1,21 @@
 const mongoose = require('mongoose');
 
 const RandomSchema = new mongoose.Schema({
-    title: String,
+    title: String, 
     image: String,
-    description: String,
+    description: String, 
     likes: {
-        type: Number,
-        default: 0,
+        type: Number, 
+        default: 0, 
     },
-    likedBy: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Account' }], 
+    likedBy: [
+        { 
+            type: mongoose.Schema.Types.ObjectId, 
+            ref: 'Account' 
+        }
+    ],
 });
 
-const RandomEntity = mongoose.model('random-collections', RandomSchema); 
+const RandomEntity = mongoose.model('random-collections', RandomSchema);
 
 module.exports = RandomEntity;
